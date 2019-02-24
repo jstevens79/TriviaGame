0112cec5c981bcc2914cfbfa975e37d64ead33bc
jstevens79 <jstevens79@users.noreply.github.com>
2019-02-23 21:57:58 -0600

    gettng questions set up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,59 +6,76 @@ var game = {
   gameStarted: false,
   score: 0,
   totalQuestions: 12,
-  currentQuestion: null,
+  currentQuestion: 0,
   currentTimer: '',
   questions: [],
-  getQuestions: function() {
-    // get the json
-    $.getJSON( "./assets/javascript/trivia-data.json", function(data) {
-
-      // get [number] random questions and push them to this.questions
-      var randomNumbers = [];
-
-      // function getNum(tot) {
-      //   if (randomNumbers.length < tot) {
-      //     var rando = Math.floor(Math.random() * data.results.length);
-      //     if (!randomNumbers.includes(rando)){
-      //       randomNumbers.push(rando)
-      //     }
-      //     getNum(tot)
-      //   }
-      // }      
-      
-      // getNum(this.totalQuestions)
-
-      while (randomNumbers.length < this.totalQuestions) {
-        var rando = Math.floor(Math.random() * data.results.length);
-        if (!randomNumbers.includes(rando)) {
-          randomNumbers.push(rando)
-        }
-      }
-      
-
-      $.each(randomNumbers, function( key, val ) {
-         console.log(val)
-      });
-
-    }.bind(this));    
-
-  },
   startGame: function() {
     // set up the initial variables
     this.loaded = false;
     this.gameStarted = false;
     this.score = 0;
     this.totalQuestions = 12;
-    this.currentQuestion = null;
+    this.currentQuestion = 0;
     this.currentTimer = '';
     this.questionsAnswered = [];
-
+    this.setupGame();
     this.getQuestions();
+  },
+  getQuestions: function() {
+    // get the json
+    $.getJSON( "./assets/javascript/trivia-data.json", function(data) {
+      var results = data.results;
+      var randomNumbers = [];
 
+      while (randomNumbers.length < this.totalQuestions) {
+        var rando = Math.floor(Math.random() * results.length);
+        if (!randomNumbers.includes(rando)) {
+          randomNumbers.push(rando)
+        }
+      }
+
+      $.each(randomNumbers, function( key, val ) {
+        var theQ = results[val];
+        theQ.answered = null;
+        game.questions.push(theQ);
+      });
+
+      this.setupQuestion()
+
+    }.bind(this));    
+
+  },
+  setupGame: function() {
+    var gameWrapper = $('<div>').addClass('gameWrapper');
+    var questionsContainer = $('<div>').addClass('questionContainer');
+    gameWrapper.append(questionsContainer);
+    $('body').prepend(gameWrapper);
+  },
+  setupQuestion: function() {
+    var theQ = this.questions[this.currentQuestion];
+    console.log(theQ)
+    var questionTxt = $('<p>').addClass('questionText').html(theQ.question);
+    var answers = [];
+
+    var correct = {answer: theQ.correct_answer, correct: true }
+    answers.push(correct);
+
+    $.each(theQ.incorrect_answers, function(ind, val){
+      var incorrect = {answer: val, correct: false}
+      answers.push(incorrect);
+    })
+
+    $('.questionContainer').append(questionTxt)
+
+    $.each(answers, function(ind, val){
+      var myAnswer = $('<div>').attr('data-answer', ind).text(val.answer);
+      $('.questionContainer').append(myAnswer)
+    })
+
+    
+    
   }
-
 }
-
 
 
 $(document).ready(function() {
